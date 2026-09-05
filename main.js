b00ad0911c0f41bf4f6a9a4aec9bf82e0595fc0b
Felipe Salgado <104802376+fesalgz@users.noreply.github.com>
2026-09-05 00:44:32 -0300

    feat: implementa ordem de serviço de entrada e saída, implementa garantia

--- main.js
+++ main.js
@@ -79,3 +79,8 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
+
+app.on('browser-window-created', (event, win) => {
+  win.maximize();
+  win.setMenuBarVisibility(false);
+});
